17dcab48b57dc1c188bf1b784c7cb574e6525992
Alex Metelli <alex-metelli@gmx.com>
2023-10-09 01:12:41 +0800

    adde point of entry

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+import { ThemedGlobalStyle, FixedGlobalStyle } from "./shared/styled/Theme";
+import AuthController from "./components/AuthController";
+
+export const AppWrapper = styled.div`
+  min-height: 100vh;
+`;
+
+function App() {
+  return (
+    <>
+      <FixedGlobalStyle />
+      <ThemedGlobalStyle />
+      <AppWrapper>
+        <AuthController />
+      </AppWrapper>
+    </>
+  );
+}
+
+export default App;
