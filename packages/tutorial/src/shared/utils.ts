4a7ad1ace42d60672ad66c376259646e7b41724a
ametel01 <alex-metelli@gmx.com>
2023-12-09 11:42:24 +0800

    updated repo and removed packages

--- packages/tutorial/src/shared/utils.ts
+++
@@ -1,5 +0,0 @@
-import { Position } from "../../../frontend/src/shared/types";
-
-export function positionToString(position: Position) {
-  return `${position.system} / ${position.orbit}`;
-}
--- packages/tutorial/src/store/tutorialState.ts
+++
@@ -1,5 +0,0 @@
-export interface TutorialState {
-  currentStep: number;
-  isCompleted: boolean;
-  // You can add other state properties as needed
-}
